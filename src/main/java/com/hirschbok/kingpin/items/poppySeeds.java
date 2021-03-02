e00032cbef18c0f44dc5161764ad3206f2e4d4ea
Hirschbock <52053770+Hirschbok@users.noreply.github.com>
2021-03-02 10:51:35 -0500

    added poppy seeds

---
+++ src/main/java/com/hirschbok/kingpin/items/poppySeeds.java
@@ -0,0 +1,11 @@
+package com.hirschbok.kingpin.items;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+
+public class poppySeeds extends Item {
+
+    public poppySeeds() {
+        super(new Item.Properties().group(ItemGroup.MATERIALS));
+    }
+}
